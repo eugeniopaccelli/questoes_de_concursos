b1ad726285ab4994986a15ba6e623c41915fcc90
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2019-09-18 13:21:49 -0300

    corrigindo o sistema!

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
 
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
 
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
 
-  devise_for :profiles
+  devise_for :users
   devise_for :admins
   
   get '/inicio', to: "site/welcome#index"
