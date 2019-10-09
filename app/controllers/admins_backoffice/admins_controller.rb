6b523b4fdeb2b35375dea79f37caaf9c3798111a
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2019-10-09 15:57:14 -0300

    Ajustando a Rota Edit Para o Admin

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -2,4 +2,7 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def index
     @admins = Admin.all
   end
+  def edit
+    @admin = Admin.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   
   namespace :admins_backoffice do
     get 'welcome/index'
-    resources :admins, only: [:index]
+    resources :admins, only: [:index, :edit]
   end
 
   namespace :users_backoffice do
