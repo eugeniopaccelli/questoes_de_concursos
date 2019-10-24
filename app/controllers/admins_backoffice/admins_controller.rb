4a789a37341bebec80ba8c4076e596c637328af1
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2019-10-24 15:20:25 -0300

    Criando o Formulário de Edição do Admin

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,8 +1,24 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  
   def index
     @admins = Admin.all
   end
   def edit
     @admin = Admin.find(params[:id])
   end
+
+  def update
+  @admin = Admin.find(params[:id])
+    if(@admin.update(admin_params))
+      redirect_to admins_backoffice_admins_path, notice: "Dados atualizados com sucesso."
+    else
+      render :edit
+    end
+  end 
+
+  private
+  def admin_params
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   
   namespace :admins_backoffice do
     get 'welcome/index'
-    resources :admins, only: [:index, :edit]
+    resources :admins, only: [:index, :edit, :update]
   end
 
   namespace :users_backoffice do
