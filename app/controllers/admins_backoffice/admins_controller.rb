3a2e1318ef8d685c67160fc6205d6c057f145198
Eugenio Oliveira <eugeniooliveira@sefaz.pi.gov.br>
2020-01-07 12:04:49 -0300

    refatorando o controller

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,19 +1,16 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+
+  before_action :verify_password, only: [:update]
+  before_action :set_admin, only: [:edit, :update]
   
   def index
     @admins = Admin.all
   end
   def edit
-    @admin = Admin.find(params[:id])
+    
   end
 
-  def update
-
-    if(params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?)
-      params[:admin].extract!(:password, :password_confirmation)
-    end
-    
-  @admin = Admin.find(params[:id])
+  def update    
     if(@admin.update(admin_params))
       redirect_to admins_backoffice_admins_path, notice: "Dados atualizados com sucesso."
     else
@@ -22,6 +19,16 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   end 
 
   private
+  def verify_password
+    if(params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?)
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+  
   def admin_params
     params.require(:admin).permit(:email, :password, :password_confirmation)
   end
