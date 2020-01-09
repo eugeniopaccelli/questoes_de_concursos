e79fb4adb77b304767440addf4965569736338e9
Eugenio Oliveira <eugeniooliveira@sefaz.pi.gov.br>
2020-01-09 12:47:10 -0300

    Apagando um administrador

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,7 +1,7 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
 
   before_action :verify_password, only: [:update]
-  before_action :set_admin, only: [:edit, :update]
+  before_action :set_admin, only: [:edit, :update, :destroy]
   
   def index
     @admins = Admin.all
@@ -30,7 +30,16 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
     else
       render :edit
     end
-  end 
+  end
+  
+  def destroy
+    if(@admin.destroy)
+      redirect_to admins_backoffice_admins_path, notice: "Perfil deletado com sucesso."
+    else
+      render :index
+    end
+  
+  end
 
   private
   def verify_password
