d0e1f7bdc7bf39b9e6be7ca82f298c35906a3c2e
Eugenio Oliveira <eugeniooliveira@sefaz.pi.gov.br>
2020-01-14 10:37:51 -0300

    Criando o controller para Subjects

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index'
     resources :admins
+    resources :subjects
   end
 
   namespace :users_backoffice do
