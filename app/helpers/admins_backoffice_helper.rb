2e964f571e25927f8ce3d6798ad34d65a40413bf
Eugenio Oliveira <eugeniooliveira@sefaz.pi.gov.br>
2020-01-16 13:24:53 -0300

    Incrementando o I18n para os Models

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,9 @@
 module AdminsBackofficeHelper
+  def translate_attribute(object = nil, attribute = nil)
+    if (object && attribute)
+      object.model.human_attribute_name(attribute)
+    else
+      "Informe os parâmetros corretamente."
+    end
+  end
 end
