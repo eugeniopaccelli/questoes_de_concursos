14247cb61b4e24fbd0384a3d0f85a86e49c774ce
Eugenio Oliveira <eugeniooliveira@sefaz.pi.gov.br>
2020-01-16 09:35:12 -0300

    Criando as views (index e delete) para as Questoes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,24 @@
 Rails.application.routes.draw do
-  
   namespace :site do
-    get 'welcome/index'
+    get "welcome/index"
   end
-  
+
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get "welcome/index"
     resources :admins
     resources :users
     resources :subjects
+    resources :questions
   end
 
   namespace :users_backoffice do
-    get 'welcome/index'
-  end  
+    get "welcome/index"
+  end
 
   devise_for :users
   devise_for :admins
-  
-  get '/inicio', to: "site/welcome#index"
 
-  root 'site/welcome#index'
-  
+  get "/inicio", to: "site/welcome#index"
+
+  root "site/welcome#index"
 end
