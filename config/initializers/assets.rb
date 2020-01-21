25118009f0f6eea5d8ef5560882e91765741b37c
Eugenio Oliveira <eugeniooliveira@sefaz.pi.gov.br>
2020-01-21 13:25:58 -0300

    Migrando o Template para o User Devise

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,9 +13,15 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # folder are already added.
 
 # /app/assets
-Rails.application.config.assets.precompile += %w( admins_backoffice.css admins_backoffice.js users_backoffice.css users_backoffice.js admin_devise.js admin_devise.css )
+Rails.application.config.assets.precompile += %w( admins_backoffice.css admins_backoffice.js users_backoffice.css users_backoffice.js )
 
 # /vendor/assets
 Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
 # /lib/assets
 Rails.application.config.assets.precompile += %w( img.jpg )
+
+# Admin Assets
+Rails.application.config.assets.precompile += %w( admin_devise.js admin_devise.css )
+
+# User Assets
+Rails.application.config.assets.precompile += %w( user_devise.js user_devise.css )
