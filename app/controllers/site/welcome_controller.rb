43b93deaf4781c68ec6fa5f09de19827d7f92eb6
Eugenio Oliveira <eugeniooliveira@sefaz.pi.gov.br>
2020-01-22 13:16:56 -0300

    fazendo a consulta funcionar

--- app/controllers/site/welcome_controller.rb
+++ app/controllers/site/welcome_controller.rb
@@ -1,5 +1,5 @@
 class Site::WelcomeController < SiteController
   def index
-    @questions = Question.includes(:answers).order(created_at: :desc).page(params[:page])
+    @questions = Question.includes(:answers).order(created_at: :desc).page(params[:page]).per(5)
   end
 end
